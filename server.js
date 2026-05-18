b5eb6ea906147cccec54567697714b096f3050d7
malekdalpirou765-hue <malekdalpirou765@gmail.com>
2026-05-18 13:52:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const path = require("path");
 
+// If Node < 18, uncomment this:
+// const fetch = require("node-fetch");
+
 const app = express();
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/translate", async (req, res) => {
   try {
     const { text, to } = req.body;
+
+    console.log("Input:", text, "Target:", to);
 
     const response = await fetch("https://libretranslate.de/translate", {
       method: "POST",
@@ -29,6 +35,7 @@ app.post("/translate", async (req, res) => {
     });
 
   } catch (err) {
+    console.error(err);
     res.json({ result: "Translation error" });
   }
 });
@@ -38,4 +45,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Running"));
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
